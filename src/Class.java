ddb8556e11414ebe8528fd44bd473da07693401c
Gamilton <110669474+Gamilton30@users.noreply.github.com>
2023-05-19 19:59:19 +0600

    Update Class.java
    2 project
    automatic score calculation
    opened doors

--- src/Class.java
+++ src/Class.java
@@ -35,3 +35,34 @@ if (passedAllDoors){
 
     }
 }
+//second project 
+//automatic score calculation
+import java.util.Scanner;
+
+public class Class1 {
+    public static void main(String args[]) {
+//automatic score calculation
+      Scanner scan=new Scanner(System.in);
+        System.out.println(" enter your grade");
+        int n= scan.nextInt();
+        if (n>=90 && n<=100){
+            System.out.println(" <A> "+" good job");
+        } else if (n>=80 && n<=90) {
+            System.out.println("<B>"+ " good");
+        }else if (n>=70 && n<=80) {
+            System.out.println("<C>"+" you must doing hw");
+        }else if (n>=60 && n<=70) {
+            System.out.println("<D>"+"no comment");
+        }else if (n>=50 && n<=60) {
+            System.out.println("<F>"+" stay after class");
+        }else if (n>=0 && n<=50) {
+            System.out.println("this number is not");
+        } else  {
+            System.out.println(" please try again ");
+
+    }
+
+    }
+
+}
+
